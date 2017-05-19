5f496ff9000c099a10265c5c8ae7cd14160ec4a4
byan1197 <Bondyan97@gmail.com>
2017-05-19 14:12:59 -0400

    finished home UI and settings UI

--- app/src/main/java/com/uottawa/bond/simpletip/Tab1Settings.java
+++ app/src/main/java/com/uottawa/bond/simpletip/Tab1Settings.java
@@ -5,6 +5,8 @@ import android.view.LayoutInflater;
 import android.support.v4.app.Fragment;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 import android.widget.TextView;
 
 /**
@@ -13,11 +15,26 @@ import android.widget.TextView;
 
 public class Tab1Settings extends Fragment{
 
+    Spinner curSpin, defaultSpin;
+    ArrayAdapter<CharSequence> aCur, aDefault;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.tab1settings, container, false);
+
+        defaultSpin = (Spinner)rootView.findViewById(R.id.defaultTip);
+        aDefault = ArrayAdapter.createFromResource(getActivity().getBaseContext(), R.array.tipArray, android.R.layout.simple_spinner_item);
+        aDefault.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        defaultSpin.setAdapter(aDefault);
+
+        curSpin = (Spinner)rootView.findViewById(R.id.currencySpinner);
+        aCur = ArrayAdapter.createFromResource(getActivity().getBaseContext(), R.array.currencyArray, android.R.layout.simple_spinner_item);
+        aCur.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        curSpin.setAdapter(aCur);
+
         return rootView;
+
     }
 
 }
--- app/src/main/java/com/uottawa/bond/simpletip/Tab2Home.java
+++ app/src/main/java/com/uottawa/bond/simpletip/Tab2Home.java
@@ -1,11 +1,13 @@
 package com.uottawa.bond.simpletip;
 
+import android.media.Rating;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.RatingBar;
 import android.widget.Spinner;
 import android.widget.TextView;
 
